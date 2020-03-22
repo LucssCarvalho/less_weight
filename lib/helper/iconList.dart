654ce9d5538d93c760e066f1a50fca163122a94d
Lucas Carvalho <lucas.carvalhocco@gmail.com>
2020-03-21 21:31:28 -0300

    add emojiList

---
+++ lib/helper/iconList.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Map<String, String> emojiList = {
+  "Status.veryBad": "😭",
+  "'Status.bad'": '😟',
+  "Status.normal": "😐",
+  'Status.happy': '🙂',
+  'Status.veryHappy': '😃',
+};
