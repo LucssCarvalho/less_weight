f9795f30f7c6150df77307bbd2fda9320d9528cc
Lucas Carvalho <lucas.carvalhocco@gmail.com>
2020-03-21 21:31:09 -0300

    add widget add weight

--- lib/add_weight.dart
+++ lib/add_weight.dart
@@ -1,34 +1,209 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:io';
 
-class AddWeight extends StatelessWidget {
+import 'package:date_format/date_format.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+class FormAddWeight extends StatefulWidget {
+  @override
+  _FormAddWeightState createState() => _FormAddWeightState();
+}
+
+enum Status { veryBad, bad, normal, happy, veryHappy }
+
+class _FormAddWeightState extends State<FormAddWeight> {
+  final _weightController = TextEditingController();
+  final _statusController = TextEditingController();
+  List _weightList = [];
+
+  Status _character = Status.normal;
+
+  @override
+  void initState() {
+    super.initState();
+    _readData().then((data) {
+      setState(() {
+        _weightList = json.decode(data);
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.greenAccent,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            title: Text(
-              'Dashboard',
-              style: TextStyle(color: Colors.black),
-            ),
-            icon: Icon(
-              Icons.dashboard,
-              color: Colors.black,
-            ),
-          ),
-          BottomNavigationBarItem(
-            title: Text(
-              "Add peso",
-              style: TextStyle(color: Colors.black),
-            ),
-            icon: Icon(
-              Icons.add,
-              color: Colors.black,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Coloque seu peso atual',
+          style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Form(
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: TextField(
+                      controller: _weightController,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: "Seu peso atual",
+                        labelStyle: TextStyle(
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              '😭',
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            Radio(
+                              value: Status.veryBad,
+                              groupValue: _character,
+                              onChanged: (Status value) {
+                                setState(() {
+                                  _character = value;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              '😟',
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            Radio(
+                              value: Status.bad,
+                              groupValue: _character,
+                              onChanged: (Status value) {
+                                setState(() {
+                                  _character = value;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              '😐',
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            Radio(
+                              value: Status.normal,
+                              groupValue: _character,
+                              onChanged: (Status value) {
+                                setState(() {
+                                  _character = value;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              '🙂',
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            Radio(
+                              value: Status.happy,
+                              groupValue: _character,
+                              onChanged: (Status value) {
+                                setState(() {
+                                  _character = value;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              '😍',
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            Radio(
+                              value: Status.veryHappy,
+                              groupValue: _character,
+                              onChanged: (Status value) {
+                                setState(() {
+                                  _character = value;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  RaisedButton(
+                      color: Colors.green,
+                      child: Text('ADD'),
+                      textColor: Colors.white,
+                      onPressed: _addTodo),
+                ],
+              ),
             ),
           ),
         ],
       ),
     );
+  }
+
+  void _addTodo() {
+    if (_weightController.text.isEmpty) return;
+    setState(() {
+      Map<String, dynamic> newTodo = Map();
+      newTodo["title"] = _weightController.text;
+      _weightController.text = "";
+      newTodo["status"] = _character.toString();
+      newTodo["data"] = formatDate(
+          DateTime.now(), [dd, '/', mm, '/', yyyy, ' | ', HH, ':', nn]);
+
+      _weightList.add(newTodo);
+      _saveData();
+    });
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_weightList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
   }
 }
